37b9cd24866f57cda6728cac41f74b30ffa46696
raj <rajk80007@gmail.com>
2024-04-28 20:28:35 +0530

    Image tag added in Hero Section

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 
 const Hero = () => {
   return (
@@ -6,15 +8,19 @@ const Hero = () => {
   <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
     <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
       <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
-        <img
+        <Image
           alt=""
-          src="doctors.jpg"
-          className="absolute inset-0 h-full w-full object-cover"
+          src="/doctors.jpg"
+          width={800}
+          height={800}
+          className="absolute inset-0 h-full w-full object-cover rounded-3xl"
         />
       </div>
 
       <div className="lg:py-24">
-        <h2 className="text-3xl font-bold sm:text-4xl">Grow your audience</h2>
+        <h2 className="text-3xl font-bold sm:text-4xl">Find & Book 
+        <span className='text-primary'>Appointment </span> with your Fav 
+        <span className='text-primary'> Doctors</span></h2>
 
         <p className="mt-4 text-gray-600">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic atque tenetur quis
@@ -22,12 +28,7 @@ const Hero = () => {
           quidem quam repellat.
         </p>
 
-        <a
-          href="#"
-          className="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
-        >
-          Get Started Today
-        </a>
+        <Button className="mt-8">Explore Now</Button>
       </div>
     </div>
   </div>
